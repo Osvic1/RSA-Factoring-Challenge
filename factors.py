664bfa8166e5285f63ebdd7b2a952d6af90565a9
Osvic1 <timothyv952@gmail.com>
2023-01-20 10:58:03 -0800

    # mission impossible for reals

---
+++ factors.py
@@ -0,0 +1,24 @@
+import sys
+
+def factorize(n):
+    for i in range(2, int(n**(1/2))+1):
+        if n%i == 0:
+            return (i, n//i)
+    return (n, 1)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: factors <file>")
+        return
+    try:
+        with open(sys.argv[1]) as f:
+            for line in f:
+                n = int(line)
+                p, q = factorize(n)
+                print(f"{n}={p}*{q}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
+
